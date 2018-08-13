824f420a1196c0808afc532420ea3e7b8c510fc7
yuvalk20-meet <yuvalk20@meet.mit.edu>
2018-08-13 04:33:12 -0400

    Stopping here

---
+++ Final Project.py
@@ -0,0 +1,17 @@
+import turtle
+
+screen = turtle.Screen()
+
+def choose_items():
+	item = screen.textinput('Welcome to the house', "What item would you like to swap?")
+
+
+        
+
+
+
+
+
+
+
+choose_items()
